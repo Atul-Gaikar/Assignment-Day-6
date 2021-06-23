b93f59cddef5542e2fb43f892af2834523a65d2a
Atul <atulgaikarag9@gmail.com>
2021-06-23 15:39:03 +0530

    Assignment Day 6

---
+++ src/Assignment/Fibonacci_Series.java
@@ -0,0 +1,24 @@
+package Assignment;
+
+public class Fibonacci_Series {
+	static void fibonicci(int number) {
+		int num1 = 0;
+		int num2 = 1;
+		int i = 0;
+		while (i < number) {
+			System.out.print(num1 + " ");
+			int num3 = num2 + num1;
+			num1 = num2;
+			num2 = num3;
+			i = i + 1;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int number = 10;
+		fibonicci(number);
+
+	}
+
+}
---
+++ src/Assignment/Perfect_Number.java
@@ -0,0 +1,30 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Perfect_Number {
+	static boolean isPerfect(int number) {
+		if (number == 1)
+			return false;
+		int sum = 1;
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				sum += i;
+			}
+		}
+		if (sum == number)
+			return true;
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the number = ");
+		int number = s.nextInt();
+		if (isPerfect(number))
+			System.out.println(number + " is perfect");
+		else
+			System.out.println(number + " not perfect");
+	}
+
+}
